8cdd526c7476ec87f3260f7e781fdaee426eb0dd
alinoisey <alinoisey@gmail.com>
2022-11-01 17:05:48 +0330

    rename varriable-class-method and delete config class

--- src/main/java/com/tosan/ComparatorTask.java
+++ src/main/java/com/tosan/ComparatorTask.java
@@ -14,14 +14,14 @@ public class ComparatorTask implements TaskType {
         final BuildLogger buildLogger = taskContext.getBuildLogger();
         String pathProject = taskContext.getWorkingDirectory().getAbsolutePath();
         buildLogger.addBuildLogEntry("task plugin started ===============");
-        Searcher searchFile = new Searcher();
-        List<String> listLogbackAddresses = searchFile.searchLogbackFiles(pathProject, "logback.xml");
+        Searcher searcher = new Searcher();
+        List<String> listLogbackAddresses = searcher.getListLogback(pathProject);
         buildLogger.addBuildLogEntry("search Logback files finished ================");
-        Comparator comparePattern = new Comparator(buildLogger);
-        List<String> listProblemFiles = comparePattern.comparePatternLogback(listLogbackAddresses);
+        Comparator comparator = new Comparator(buildLogger);
+        List<String> listProblemLogback = comparator.comparePatternLogback(listLogbackAddresses);
         buildLogger.addBuildLogEntry("compare pattern for logback files finished ===================");
         Creator resultCompare = new Creator(buildLogger);
-        resultCompare.createResultCompareFile(listProblemFiles, pathProject);
+        resultCompare.createResultCompareFile(listProblemLogback, pathProject);
         buildLogger.addBuildLogEntry("task plugin finished ====================");
         return TaskResultBuilder.newBuilder(taskContext).success().build();
     }
--- src/main/java/com/tosan/Searcher.java
+++ src/main/java/com/tosan/Searcher.java
@@ -1,27 +1,38 @@
 package com.tosan;
 
+import org.springframework.web.bind.annotation.RestController;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Searcher {
     private List<String> addressList = new ArrayList();
+    private final String fileName="logback.xml";
 
-    public List<String> searchLogbackFiles(String path, String fileName) {
-        List<String> blackListConfig =Config.getBlackListDirecrctory();
+    private List<String> getBlackList(){
+        final List<String> BLACKLIST=new ArrayList<>();
+        BLACKLIST.add("target");
+        BLACKLIST.add("values");
+
+        return BLACKLIST;
+    }
+
+    public List<String> getListLogback(String path) {
+//        List<String> blackListConfig =Config.getBlackListDirecrctory();
         File searchPath = new File(path);
         File[] listFiles = searchPath.listFiles();
         for (File file : listFiles) {
             if (file.isDirectory()) {
                 boolean blackListOf = false;
-                for (int i = 0; i < blackListConfig.size(); i++) {
-                    if (file.getName().equalsIgnoreCase(blackListConfig.get(i))) {
+                for (int i = 0; i < getBlackList().size(); i++) {
+                    if (file.getName().equalsIgnoreCase(getBlackList().get(i))) {
                         blackListOf = true;
                         break;
                     }
                 }
                 if (!blackListOf) {
-                    searchLogbackFiles(file.getAbsolutePath(), fileName);
+                    getListLogback(file.getAbsolutePath());
                 }
             } else {
                 if (file.getName().matches(fileName)) {
